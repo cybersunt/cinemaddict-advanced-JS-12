b64cb7ffbba57383e7d0a7db3bb056f01806a47d
Maksimova, Yuliya <Yuliya.Maksimova@nexign.com>
2020-07-29 13:19:17 +0300

    feature: render main templates

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-// Header components
+// Header Components
 const createUserInfoTemplate = () => {
   return (
     `<section class="header__profile profile">
@@ -14,7 +14,32 @@ const createUserInfoTemplate = () => {
   );
 };
 
-// Footer components
+// Main Components
+const createSiteMenuTemplate = () => {
+  return (
+    `<nav class="main-navigation">
+      <div class="main-navigation__items">
+        <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
+        <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
+        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
+        <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+      </div>
+      <a href="#stats" class="main-navigation__additional">Stats</a>
+    </nav>`
+  );
+};
+const createSortTemplate = () => {
+  return (
+    `<ul class="sort">
+      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
+      <li><a href="#" class="sort__button">Sort by date</a></li>
+      <li><a href="#" class="sort__button">Sort by rating</a></li>
+    </ul>`
+  );
+};
+const createMoviesTemplate = () => `<section class="films"></section>`;
+
+// Footer Components
 const createMovieStatsTemplate = () => {
   return (
     `<section class="footer__statistics">
@@ -24,7 +49,11 @@ const createMovieStatsTemplate = () => {
 };
 
 const siteHeaderElement = document.querySelector(`.header`);
+const siteMainElement = document.querySelector(`.main`);
 const siteFooterElement = document.querySelector(`.footer`);
 
 render(siteHeaderElement, createUserInfoTemplate(), `beforeend`);
+render(siteMainElement, createSiteMenuTemplate(), `beforeend`);
+render(siteMainElement, createSortTemplate(), `beforeend`);
+render(siteMainElement, createMoviesTemplate(), `beforeend`);
 render(siteFooterElement, createMovieStatsTemplate(), `beforeend`);
