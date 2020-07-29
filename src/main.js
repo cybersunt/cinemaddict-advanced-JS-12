4e01d5e8ef4a72f197f3d387b01986859bd3bede
Maksimova, Yuliya <Yuliya.Maksimova@nexign.com>
2020-07-29 14:34:27 +0300

    feature: render main movies list with five movie cards

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const MOVIES_LIST_COUNT = 3;
+const MOVIES_COUNT = 5;
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -56,6 +57,29 @@ const createMoviesListTitleTemplate = (title, hidden = false) => {
 const createMoviesListContainerTemplate = ()=> `<div class="films-list__container"></div>`;
 const createLoadMoreButtonTemplate = ()=> `<button class="films-list__show-more">Show more</button>`;
 
+// Movie Components
+const createMovieCard = () => {
+  return (
+    `<article class="film-card">
+      <h3 class="film-card__title">The Dance of Life</h3>
+      <p class="film-card__rating">8.3</p>
+      <p class="film-card__info">
+         <span class="film-card__year">1929</span>
+         <span class="film-card__duration">1h 55m</span>
+         <span class="film-card__genre">Musical</span>
+       </p>
+      <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
+      <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
+      <a class="film-card__comments">5 comments</a>
+      <form class="film-card__controls">
+        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
+        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
+        <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+      </form>
+    </article>`
+  );
+};
+
 // Footer Components
 const createMovieStatsTemplate = () => {
   return (
@@ -83,6 +107,12 @@ for (let i = 0; i < MOVIES_LIST_COUNT; i++) {
 
 const mainMoviesListElement = moviesElement.querySelector(`.films-list`);
 
-render(mainMoviesListElement, createMoviesListTitleTemplate('All movies. Upcoming', true), `beforeend`);
+render(mainMoviesListElement, createMoviesListTitleTemplate(`All movies. Upcoming`, true), `beforeend`);
 render(mainMoviesListElement, createMoviesListContainerTemplate(), `beforeend`);
 render(mainMoviesListElement, createLoadMoreButtonTemplate(), `beforeend`);
+
+const moviesListContainerElement = mainMoviesListElement.querySelector(`.films-list__container`);
+
+for (let i = 0; i < MOVIES_COUNT; i++) {
+  render(moviesListContainerElement, createMovieCard(), `beforeend`);
+}
