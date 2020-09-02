ff2cb21f2c2a697bec7aaceefeec27d53217e59f
Yulia <juliam2007@mail.ru>
2020-09-02 22:24:29 +0300

    refactor: move component functions outside the parent component

--- src/view/movie-card-details.js
+++ src/view/movie-card-details.js
@@ -2,32 +2,10 @@ import {EMOJI} from "../const";
 import {getCommentDate, getPictureUrl, getReleaseDate, getRuntimeInHours, getStringFromArray} from "../utils/movie";
 import Abstract from "./abstract";
 
-const createMovieCardDetailsTemplate = (movie)=> {
+const createMovieCardDetailsInfoHeadTemplate = ({title, originalTitle, rating}) => {
 
-  const {
-    poster,
-    title,
-    originalTitle,
-    description,
-    releaseDate,
-    director,
-    writers,
-    actors,
-    runtime,
-    country,
-    genres,
-    rating,
-    ageLimitations,
-    comments,
-    isWatchlist,
-    isHistory,
-    isFavorite
-  } = movie;
-
-  const createMovieCardDetailsInfoHeadTemplate = () => {
-
-    return (
-      `<div class="film-details__info-head">
+  return (
+    `<div class="film-details__info-head">
         <div class="film-details__title-wrap">
           <h3 class="film-details__title">${title}</h3>
           <p class="film-details__title-original">${originalTitle}</p>
@@ -37,23 +15,33 @@ const createMovieCardDetailsTemplate = (movie)=> {
           <p class="film-details__total-rating">${rating.toPrecision(2)}</p>
         </div>
       </div>`
-    );
-  };
+  );
+};
 
-  const createMovieCardDetailsTableRowTemplate = (term, value) => {
-    return (
-      `<tr class="film-details__row">
+const createMovieCardDetailsTableRowTemplate = (term, value) => {
+  return (
+    `<tr class="film-details__row">
         <td class="film-details__term">${term}</td>
         <td class="film-details__cell">${value}</td>
       </tr>`
-    );
-  };
+  );
+};
 
-  const createMovieCardDetailsGenresTemplate = () => {
-    return genres.map((genre) =>
-      `<span class="film-details__genre">${genre}</span>`
-    ).join(``);
-  };
+const createMovieCardDetailsGenresTemplate = ({genres}) => {
+  return genres.map((genre) =>
+    `<span class="film-details__genre">${genre}</span>`
+  ).join(``);
+};
+
+const createMovieCardDetailsTableTemplate = (movie) => {
+  const {
+    releaseDate,
+    director,
+    writers,
+    actors,
+    runtime,
+    country
+  } = movie;
 
   const detailsData = [
     [`Director`, director],
@@ -62,23 +50,22 @@ const createMovieCardDetailsTemplate = (movie)=> {
     [`Release Date`, getReleaseDate(releaseDate)],
     [`Runtime`, getRuntimeInHours(runtime)],
     [`Country`, country],
-    [`Genres`, createMovieCardDetailsGenresTemplate()]
+    [`Genres`, createMovieCardDetailsGenresTemplate(movie)]
   ];
 
-  const createMovieCardDetailsTableTemplate = () => {
-    const content = detailsData
-      .map(([name, value]) => createMovieCardDetailsTableRowTemplate(name, value))
-      .join(``);
-    return (
-      `<table class="film-details__table">
+  const content = detailsData
+    .map(([name, value]) => createMovieCardDetailsTableRowTemplate(name, value))
+    .join(``);
+  return (
+    `<table class="film-details__table">
         ${content}
       </table>`
-    );
-  };
+  );
+};
 
-  const createMovieCardDetailsCommentsListTemplate = () => {
-    const content = comments.map((comment) =>
-      `<li class="film-details__comment">
+const createMovieCardDetailsCommentsListTemplate = (comments) => {
+  const content = comments.map((comment) =>
+    `<li class="film-details__comment">
         <span class="film-details__comment-emoji">
 
           <img src="${getPictureUrl(`emoji`, comment.emoji)}.png" width="55" height="55" alt="emoji-smile">
@@ -92,27 +79,27 @@ const createMovieCardDetailsTemplate = (movie)=> {
           </p>
         </div>
       </li>`
-    ).join(``);
-    return (
-      `<ul class="film-details__comments-list">${content}</ul>`
-    );
-  };
+  ).join(``);
+  return (
+    `<ul class="film-details__comments-list">${content}</ul>`
+  );
+};
 
-  const createMovieCardDetailsEmojiListTemplate = () => {
-    const content = EMOJI.map((emoji) =>
-      `<input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-${emoji}" value="${emoji}">
+const createMovieCardDetailsEmojiListTemplate = () => {
+  const content = EMOJI.map((emoji) =>
+    `<input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-${emoji}" value="${emoji}">
         <label class="film-details__emoji-label" for="emoji-smile">
           <img src="${getPictureUrl(`emoji`, emoji)}.png" width="30" height="30" alt="emoji">
           </label>`
-    ).join(``);
-    return (
-      `<div class="film-details__emoji-list">${content}</div>`
-    );
-  };
+  ).join(``);
+  return (
+    `<div class="film-details__emoji-list">${content}</div>`
+  );
+};
 
-  const createMovieCardDetailsNewCommentTemplate = () => {
-    return (
-      `<div class="film-details__new-comment">
+const createMovieCardDetailsNewCommentTemplate = () => {
+  return (
+    `<div class="film-details__new-comment">
         <div for="add-emoji" class="film-details__add-emoji-label"></div>
 
          <label class="film-details__comment-label">
@@ -120,28 +107,22 @@ const createMovieCardDetailsTemplate = (movie)=> {
          </label>
          ${createMovieCardDetailsEmojiListTemplate()}
       </div>`
-    );
-  };
+  );
+};
 
-  const createMovieCardDetailsCommentsTemplate = () => {
-    return (
-      `<section class="film-details__comments-wrap">
+const createMovieCardDetailsCommentsTemplate = (comments) => {
+  return (
+    `<section class="film-details__comments-wrap">
         <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${comments.length}</span></h3>
-        ${createMovieCardDetailsCommentsListTemplate()}
+        ${createMovieCardDetailsCommentsListTemplate(comments)}
         ${createMovieCardDetailsNewCommentTemplate()}
       </section>`
-    );
-  };
+  );
+};
 
-  const movieStatusControls = [
-    [`watchlist`, `Add to watchlist`, isWatchlist],
-    [`watched`, `Already watched`, isHistory],
-    [`favorite`, `Add to favorites`, isFavorite],
-  ];
-
-  const createMovieCardControlTemplate = (status, label, isChecked) => {
-    return (
-      `<input
+const createMovieCardControlTemplate = (status, label, isChecked) => {
+  return (
+    `<input
         type="checkbox"
         class="film-details__control-input visually-hidden"
         id="${status}"
@@ -149,19 +130,36 @@ const createMovieCardDetailsTemplate = (movie)=> {
         ${isChecked ? `checked` : ``}
       >
       <label for="${status}" class="film-details__control-label film-details__control-label--${status}">${label}</label>`
-    );
-  };
+  );
+};
 
-  const createMovieCardControlsTemplate = () => {
-    const content = movieStatusControls
-      .map(([status, label, isChecked]) => createMovieCardControlTemplate(status, label, isChecked))
-      .join(``);
-    return (
-      `<section class="film-details__controls">
+const createMovieCardControlsTemplate = (isWatchlist, isHistory, isFavorite) => {
+  const movieStatusControls = [
+    [`watchlist`, `Add to watchlist`, isWatchlist],
+    [`watched`, `Already watched`, isHistory],
+    [`favorite`, `Add to favorites`, isFavorite],
+  ];
+
+  const content = movieStatusControls
+    .map(([status, label, isChecked]) => createMovieCardControlTemplate(status, label, isChecked))
+    .join(``);
+  return (
+    `<section class="film-details__controls">
         ${content}
       </section>`
-    );
-  };
+  );
+};
+
+const createMovieCardDetailsTemplate = (movie)=> {
+
+  const {
+    poster,
+    description,
+    ageLimitations,
+    comments,
+    isWatchlist,
+    isHistory,
+    isFavorite} = movie;
 
   return (
     `<section class="film-details">
@@ -178,19 +176,19 @@ const createMovieCardDetailsTemplate = (movie)=> {
         </div>
         <div class="film-details__info">
 
-          ${createMovieCardDetailsInfoHeadTemplate()}
+          ${createMovieCardDetailsInfoHeadTemplate(movie)}
 
-          ${createMovieCardDetailsTableTemplate()}
+          ${createMovieCardDetailsTableTemplate(movie)}
 
           <p class="film-details__film-description">${getStringFromArray(description, `.`)}</p>
         </div>
       </div>
 
-      ${createMovieCardControlsTemplate()}
+      ${createMovieCardControlsTemplate(isWatchlist, isHistory, isFavorite)}
     </div>
 
     <div class="form-details__bottom-container">
-      ${createMovieCardDetailsCommentsTemplate()}
+      ${createMovieCardDetailsCommentsTemplate(comments)}
     </div>
   </form>
 </section>`
